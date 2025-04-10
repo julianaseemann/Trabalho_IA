ba8b32aadb922de87c28e18c65559a665091e35a
Juliana Seemann <131565013+julianaseemann@users.noreply.github.com>
2025-04-10 20:24:23 -0300

    Add files via upload

--- Trabalho_ÌA/bayes.py
+++ Trabalho_ÌA/bayes.py
@@ -1,6 +1,11 @@
 # iMPORTAR AS BIBLIOTECAS
 import streamlit as st
 import fitz
+from groq import Groq
+
+# Cofigufigurar chave da Groq
+# GROQ_API_KEY = "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"
+cliente = Groq(api_key = GROQ_API_KEY)
 
 # função para extrair os arquivos     
 def extract_files(uploader):
@@ -11,7 +16,13 @@ def extract_files(uploader):
                 text += page.get_text("text") 
     return text
     
-    
+def chat_with_groq(prompt, context):
+    response = client.chat.completions.create(
+        model-"1lama-3.3-70b-versatile", 
+        messages= [
+            {"role": "system","content": "Você é um assistente que responde com b.
+            {"role": "user","content": f"{context) \n\Pergunta: (prompt) "}
+
 # CRIAR A INTERFACE
 def main():
     st.title("O nome do meu sistema inteligente")
